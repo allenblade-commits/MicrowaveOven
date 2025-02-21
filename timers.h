7ba3f8ed273a41944e6c8366f72279bf2048616b
ALLEN STANLEY <156685117+allenblade-commits@users.noreply.github.com>
2025-02-21 09:45:27 +0530

    Update timers.h

--- timers.h
+++ timers.h
@@ -1,15 +1,20 @@
 /* 
- * File:   timers.h
+ * File: timers.h
  * Author: CFT
  *
  * Created on 28 April, 2020, 6:42 PM
+ * 
+ * Description:
+ * This header file contains function prototypes for initializing Timer0 and Timer2.
  */
 
 #ifndef TIMERS_H
-#define	TIMERS_H
+#define TIMERS_H
 
+/* Function to initialize Timer0 */
 void init_timer0(void);
+
+/* Function to initialize Timer2 */
 void init_timer2(void);
 
-#endif	/* TIMERS_H */
-
+#endif /* TIMERS_H */
