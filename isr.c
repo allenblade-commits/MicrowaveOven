39e244953e59ee1c701d0a6d1bced03c4ca11215
ALLEN STANLEY <156685117+allenblade-commits@users.noreply.github.com>
2025-02-21 09:40:02 +0530

    Update isr.c

--- isr.c
+++ isr.c
@@ -1,24 +1,38 @@
-#include <xc.h>
-#include "main.h"
+#include <xc.h>   // Include compiler-specific header file
+#include "main.h" // Include main project header file
+
+// External variables for tracking time (declared in another source file)
 extern unsigned char sec, min;
 
+/* 
+ * Interrupt Service Routine (ISR)
+ * Handles Timer2 Interrupt for a countdown timer
+ */
 void __interrupt() isr(void)
 {
-    static unsigned int count = 0;
+    static unsigned int count = 0;  // Static variable to maintain count across ISR calls
     
+    // Check if Timer2 interrupt flag is set
     if (TMR2IF == 1)
     {    
-        if (++count == 1250) // 1sec
+        // Increment count; 1250 interrupts correspond to 1 second
+        if (++count == 1250) // Assuming Timer2 overflows at 0.8ms interval
         {
-            count = 0;
-            if(sec > 0){
-                sec--;
-            }else if((sec == 0) && (min > 0)){
-                min--;
-                sec = 59;
+            count = 0; // Reset count every second
+            
+            // Countdown logic
+            if (sec > 0)
+            {
+                sec--; // Decrease seconds
+            }
+            else if ((sec == 0) && (min > 0))
+            {
+                min--; // Decrease minutes when seconds reach 0
+                sec = 59; // Reset seconds to 59
             }
         }
         
+        // Clear Timer2 interrupt flag to acknowledge interrupt
         TMR2IF = 0;
     }
 }
